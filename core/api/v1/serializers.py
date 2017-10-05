1341d6f3be3ffe249d164faf335ab21a72fbe59a
Wunder9l <vvarty@mail.ru>
2017-10-06 00:01:54 +0300

    Change api fields for like model

--- core/api/v1/serializers.py
+++ core/api/v1/serializers.py
@@ -5,9 +5,16 @@ from core.serializers import ModelWithAuthorSerializer
 
 
 class LikeSerializer(ModelWithAuthorSerializer):
+    object = serializers.ReadOnlyField(source='object.get_title_for_like')
+    content_type = serializers.SerializerMethodField(read_only=True)
+
+    def get_content_type(self, like_object):
+        type_str = str(like_object.object.__class__)
+        return type_str[type_str.find("'") + 1:type_str.rfind("'")]
+
     class Meta:
         model = Like
-        fields = '__all__'
+        fields = 'author', 'created', 'updated', 'object', 'content_type'
 
 
 class UserSerializer(serializers.ModelSerializer):
--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 
 
-class ModelWithAuthorSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.id')
+class ModelWithAuthorSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
 
+    # author_url = serializers.HyperlinkedIdentityField(view_name='id')
     class Meta:
         abstract = True
